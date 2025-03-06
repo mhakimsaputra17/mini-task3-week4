a55eabb3f7b5769fc846f034512431fb361bb92c
M Hakim Saputra <a1011201296@student.untan.ac.id>
2025-03-06 17:43:57 +0700

    fix:error when post data

--- script2.js
+++ script2.js
@@ -1,29 +1,39 @@
 
 const f = document.querySelector("myformku");
-const titleData = f.value
-const bodyData= f.value
 
 
-const objOption = {
-    method :"POST",
-    body  :JSON.stringify({
-        title : titleData,
-        body : bodyData,
-        userId:1
-    }),
-    headers : {
-        "Content-Type" : "application/json"
-    }
-}
+// const objOption = {
+//     method :"POST",
+//     body  :JSON.stringify({
+//         title : titleData,
+//         body : bodyData,
+//         userId:1
+//     }),
+//     headers : {
+//         "Content-Type" : "application/json"
+//     }
+// }
 
 // getData();
 // const form = document.querySelector("form")
 const btn = document.querySelector("form-example")
 btn.addEventListener("click", async(e)=>{
         try{
-            
+            const title = e.target.title.value;
+            const body = e.target.body.value;
+
             const url = "https://jsonplaceholder.typicode.com/posts";
-            const response = await fetch(url ,objOption);
+            const response = await fetch(url ,{
+                method :"POST",
+                body  :JSON.stringify({
+                    title : title,
+                    body : body,
+                    userId:1
+                }),
+                headers : {
+                    "Content-Type" : "application/json"
+                }
+            });
             if (!response.ok){
                 throw new Error(response.statusText);
             }
